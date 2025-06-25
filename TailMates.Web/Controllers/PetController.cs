65534c35666c38c76ecbccfe21babb650dbffd2f
Adrian Gudjev <adrian3829@abv.bg>
2025-06-25 10:58:30 +0300

    Implement pet controller logic

---
+++ TailMates.Web/Controllers/PetController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TailMates.Services.Core.Interfaces;
+using TailMates.Web.ViewModels.Pet;
+
+namespace TailMates.Web.Controllers
+{
+    public class PetController : Controller
+    {
+		private readonly IPetService petService;
+
+		public PetController(IPetService petService)
+		{
+			this.petService = petService;
+		}
+
+		public async Task<IActionResult> All()
+        {
+			var petViewModels = await petService.GetAllPetsAsync();
+			var viewModel = new PetListViewModel
+			{
+				Pets = petViewModels
+			};
+
+			return View(viewModel);
+		}
+    }
+}
