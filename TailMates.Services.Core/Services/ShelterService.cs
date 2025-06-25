ebe3d63ed460fdd7058278016be4f8787c005dcc
Adrian Gudjev <adrian3829@abv.bg>
2025-06-25 11:12:37 +0300

    Add Shelter service

---
+++ TailMates.Services.Core/Services/ShelterService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TailMates.Data;
+using TailMates.Services.Core.Interfaces;
+using TailMates.Web.ViewModels.Shelter;
+
+namespace TailMates.Services.Core.Services
+{
+	public class ShelterService : IShelterService
+	{
+		private readonly TailMatesDbContext dbContext;
+
+		public ShelterService(TailMatesDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<IEnumerable<ShelterViewModel>> GetAllSheltersAsync()
+		{
+			var shelters = await dbContext.Shelters
+			   .ToListAsync();
+
+			
+			var shelterViewModels = shelters.Select(s => new ShelterViewModel
+			{
+				Id = s.Id,
+				Name = s.Name,
+				Address = s.Address,
+				PhoneNumber = s.PhoneNumber,
+				Email = s.Email
+			}).ToList();
+
+			return shelterViewModels;
+		}
+	}
+}
