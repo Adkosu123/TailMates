ebd71c4b518a1ef342c0ab1ba81693a7c07bd7b6
Adrian Gudjev <adrian3829@abv.bg>
2025-06-25 11:12:25 +0300

    Added Shelter interface

---
+++ TailMates.Services.Core/Interfaces/IShelterService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TailMates.Web.ViewModels.Shelter;
+
+namespace TailMates.Services.Core.Interfaces
+{
+    public interface IShelterService
+    {
+		Task<IEnumerable<ShelterViewModel>> GetAllSheltersAsync();
+	}
+}
