53f324a1d8209bdb4168a1b14970c6057ce23553
Adrian Gudjev <adrian3829@abv.bg>
2025-06-25 11:13:12 +0300

    Add Shelter ViewModels

---
+++ TailMates.Web.ViewModels/Shelter/ShelterListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailMates.Web.ViewModels.Shelter
+{
+   public class ShelterListViewModel
+    {
+		public IEnumerable<ShelterViewModel> Shelters { get; set; }
+			= new List<ShelterViewModel>();
+	}
+}
---
+++ TailMates.Web.ViewModels/Shelter/ShelterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailMates.Web.ViewModels.Shelter
+{
+   public class ShelterViewModel
+    {
+		public int Id { get; set; }
+
+		[Display(Name = "Shelter Name")]
+		public string Name { get; set; } = string.Empty;
+
+		public string Address { get; set; } = string.Empty;
+
+		[Display(Name = "Phone Number")]
+		public string? PhoneNumber { get; set; }
+
+		public string? Email { get; set; }
+	}
+}
