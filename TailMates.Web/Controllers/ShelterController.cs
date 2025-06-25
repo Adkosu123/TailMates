dc7b3b6d7b4fd4c703ac101fadc0a37c12d10a0f
Adrian Gudjev <adrian3829@abv.bg>
2025-06-25 11:13:04 +0300

    Add Shelter Controller

---
+++ TailMates.Web/Controllers/ShelterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TailMates.Services.Core.Interfaces;
+using TailMates.Services.Core.Services;
+using TailMates.Web.ViewModels.Shelter;
+
+namespace TailMates.Web.Controllers
+{
+    public class ShelterController : Controller
+    {
+        private readonly IShelterService shelterService;
+
+		public ShelterController(IShelterService shelterService)
+		{
+            this.shelterService = shelterService;
+        }
+
+		public async Task<IActionResult> All()
+		{
+			
+			var shelterViewModels = await shelterService.GetAllSheltersAsync();
+
+			
+			var viewModel = new ShelterListViewModel
+			{
+				Shelters = shelterViewModels
+			};
+
+			return View(viewModel);
+		}
+    }
+}
