4716f65614b2b60152d9d3e62d4c29cb06e2f421
Adrian Gudjev <adrian3829@abv.bg>
2025-06-26 11:20:39 +0300

    Pet Details view and viewmodels

---
+++ TailMates.Web.ViewModels/Pet/AdoptionApplicationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailMates.Web.ViewModels.Pet
+{
+   public class AdoptionApplicationViewModel
+    {
+		public int Id { get; set; }
+
+		[Display(Name = "Application Date")]
+		[DataType(DataType.Date)]
+		public DateTime ApplicationDate { get; set; }
+
+		public string Status { get; set; } = string.Empty; 
+
+		[Display(Name = "Applicant Name")]
+		public string ApplicantName { get; set; } = string.Empty;
+
+		public string? ApplicantNotes { get; set; }
+		public string? AdminNotes { get; set; }
+	}
+}
---
+++ TailMates.Web.ViewModels/Pet/PetDetailsViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailMates.Web.ViewModels.Pet
+{
+	public class PetDetailsViewModel
+	{
+
+		public int Id { get; set; }
+
+		[Display(Name = "Pet Name")]
+		public string Name { get; set; } = string.Empty;
+
+		[Display(Name = "Age")]
+		public int Age { get; set; }
+
+		public string Description { get; set; } = string.Empty;
+
+		[Display(Name = "Image")]
+		public string? ImageUrl { get; set; }
+
+		public string Gender { get; set; } = string.Empty;
+
+		[Display(Name = "Listed On")]
+		[DataType(DataType.Date)]
+		public DateTime DateListed { get; set; }
+
+		[Display(Name = "Is Adopted?")]
+		public bool IsAdopted { get; set; }
+
+		[Display(Name = "Species")]
+		public string SpeciesName { get; set; } = string.Empty;
+
+		[Display(Name = "Breed")]
+		public string BreedName { get; set; } = string.Empty;
+
+		[Display(Name = "Shelter Name")]
+		public string ShelterName { get; set; } = string.Empty;
+
+		[Display(Name = "Shelter Address")]
+		public string ShelterAddress { get; set; } = string.Empty;
+
+		[Display(Name = "Shelter Phone")]
+		public string? ShelterPhoneNumber { get; set; }
+
+		[Display(Name = "Shelter Email")]
+		public string? ShelterEmail { get; set; }
+
+		public int ShelterId { get; set; }
+
+		public ICollection<AdoptionApplicationViewModel> AdoptionApplications { get; set; }
+			  = new HashSet<AdoptionApplicationViewModel>();
+	}
+}
