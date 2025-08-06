7a9b7653ccbd55c3100ce890aa5e4646a44bc9d0
Adrian Gudjev <adrian3829@abv.bg>
2025-08-06 09:52:53 +0300

    Make description non-nullable

--- TailMates.Data.Models/Shelter.cs
+++ TailMates.Data.Models/Shelter.cs
@@ -8,7 +8,7 @@
 
 		public string Address { get; set; } = null!;
 
-		public string Description { get; set; }
+		public string Description { get; set; } = null!;
 
 		public string? PhoneNumber { get; set; }
 
